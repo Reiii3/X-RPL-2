9cb41cf98d8a9e26088612cc7dce7dcaa65d9d05
Jaa <mohtoyib79@gmail.com>
2025-07-20 16:17:03 +0700

    update webroot/index.js

--- webroot/index.js
+++ webroot/index.js
@@ -1,1 +1,25 @@
-
+class MainWeb {
+  constructor(){
+    this.developer = "Reii";
+  }
+  initDoxument() {
+    this.idBtnNavigasi = document.getElementById("btn-navigasi")
+    this.eventList();
+    this.navigasiStats = true;
+  }
+  eventList() {
+    this.idBtnNavigasi.addEventListener("click", () => {
+      const idLayoutNavigasiHidden = document.querySelector('navigasi-hidden');
+      const height = idLayoutNavigasiHidden.scrollHeight + 'px';
+      const open = idLayoutNavigasiHidden.style.height = height;
+      const close = () => {
+        idLayoutNavigasiHidden.style.height = height;
+        requestAnimationFrame(() => {
+          idLayoutNavigasiHidden.style.height = "0";
+        });
+        this.navigasiStats ? open : close();
+        this.navigasiStats = !this.navigasiStats;
+      };
+    });
+  }
+}
